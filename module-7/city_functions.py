4948372043fa4221501b2dbb7b482e0cc392bd1c
Caleb <cschumacher7777777@gmail.com>
2026-02-14 21:24:39 -0600

    Changed *args into parameters population and language as None type. Added checks to if population or language was added to append to returned value

--- module-7/city_functions.py
+++ module-7/city_functions.py
@@ -1,11 +1,15 @@
-def city_country(city, country, *args):
-    
-    return f"{city}, {country} {', '.join(args)}"
+def city_country(city, country, population=None, language=None):
+    parts = [f"{city}, {country}"]
+    if population:
+        parts.append(f"Population: {population}")
+    if language:
+        parts.append(f"Language: {language}")
+    return ", ".join(parts)
 
 
 
 print(city_country("Paris", "France"))
-print(city_country("Tokyo", "Japan", "Population: 37 million"))
-print(city_country("New York", "United States", "Population: 8.4 million", "Language: English"))
+print(city_country("Tokyo", "Japan", population="37 million"))
+print(city_country("New York", "United States", population="8.4 million", language="English"))
 print(city_country("Sydney", "Australia"))
-print(city_country("Barcelona", "Spain", "Population: 1.6 million"))
+print(city_country("Barcelona", "Spain", population="1.6 million"))
